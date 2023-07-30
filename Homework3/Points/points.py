61859d2e80abc4945cb3cfcd6392c82cddf3aa02
Areg2001 <107401750+Areg2001@users.noreply.github.com>
2023-07-30 14:36:48 +0400

    Update points.py
    Adding Point4D and Point5D

--- Homework3/Points/points.py
+++ Homework3/Points/points.py
@@ -38,3 +38,58 @@ class Point3D(metaclass=SlottedStruct):
 
     def distance(self):
         return math.sqrt(pow(self.x, 2) + pow(self.y, 2) + pow(self.z, 2))
+
+    
+class Point4D(metaclass=Slot):
+​
+    __slots__ = ('_x', '_y', '_z', '_k')
+    dimension = 4
+​
+    @property
+    def x(self):
+        return self._x
+​
+    @property
+    def y(self):
+        return self._y
+​
+    @property
+    def z(self):
+        return self._z
+​
+    @property
+    def k(self):
+        return self._k
+
+    def distance(self):
+        return math.sqrt(pow(self.x, 2) + pow(self.y, 2) + pow(self.z, 2) + pow(self.k, 2))
+​    
+
+​
+class Point5D(metaclass=Slot):
+​
+    __slots__ = ('_x', '_y', '_z', '_k', '_i')
+    dimension = 5
+​
+    @property
+    def x(self):
+        return self._x
+​
+    @property
+    def y(self):
+        return self._y
+​
+    @property
+    def z(self):
+        return self._z
+​
+    @property
+    def k(self):
+        return self._k
+    
+    @property
+    def n(self):
+        return self._n
+
+    def distance(self):
+        return math.sqrt(pow(self.x, 2) + pow(self.y, 2) + pow(self.z, 2) + pow(self.k, 2) + pow(self.n, 2))
